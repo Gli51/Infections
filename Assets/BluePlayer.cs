5c5e0e2ac161f9592249c11acd88059a1881cd01
Hanke Chen <i@kokecacao.me>
2023-01-25 11:21:34 -0500

    :sparkles: add blue and red player

--- Assets/BluePlayer.cs
+++ Assets/BluePlayer.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class Player : MonoBehaviour
+public class BluePlayer : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Game game;
---
+++ Assets/RedPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class RedPlayer : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Game game;
+    public float maxSpeed;
+    private void Awake()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody2D>();
+
+        game = FindObjectOfType<Game>();
+    }
+
+
+    private void Update()
+    {
+      // detect mouse input
+      if (Input.GetMouseButtonDown(0))
+      {
+        // get mouse position
+        Vector3 mousePos = Input.mousePosition;
+        // convert mouse position to world position
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        // get direction from player to mouse position
+        Vector3 direction = (worldPos - transform.position).normalized;
+        // apply force in direction of mouse
+        rb.AddForce(direction * maxSpeed);
+      }
+    }
+
+    private void OnEnable()
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+      
+    }
+
+}
