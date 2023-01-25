a4632a6cd4c7c9356265f9f33e87983b4f8f0e4a
Hanke Chen <i@kokecacao.me>
2023-01-25 12:47:37 -0500

    :bug: fix duplicated clone

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -5,40 +5,45 @@ public enum GameState { START, REDTURN, BLUETURN, END }
 
 public class Game : MonoBehaviour
 {
-    public Player bluePlayer;
-    public Player redPlayer;
-    public InfectableCell[] cell;
-    public GameObject gameOverUI;
+  public Player bluePlayer;
+  public Player redPlayer;
+  public InfectableCell[] cell;
+  public GameObject gameOverUI;
 
 
-    public Vector3 blueSpawn;
-    public Vector3 redSpawn;
+  public Vector3 blueSpawn;
+  public Vector3 redSpawn;
 
-    public int blueScore = 0;
-    public int redScore = 0;
+  public int blueScore = 0;
+  public int redScore = 0;
 
-    public GameState state = GameState.BLUETURN;
+  public GameState state = GameState.BLUETURN;
 
-    //executed at the beginning
-    private void Start()
+  //executed at the beginning
+  private void Start()
+  {
+
+    SetupGame();
+
+  }
+
+  private void SetupGame()
+  {
+    if (bluePlayer == null)
     {
-  
-        SetupGame();
-
+      Debug.LogWarning("Blue player is not set");
     }
-
-    private void SetupGame()
+    if (redPlayer == null)
     {
-        //Instantiate red and blue players
-        Instantiate(bluePlayer, blueSpawn, Quaternion.identity);
-        bluePlayer.infectedState = 1; // this is blue player
-        Instantiate(redPlayer, redSpawn, Quaternion.identity);
-        redPlayer.infectedState = 2; // this is red player
-        
+      Debug.LogWarning("Red player is not set");
     }
+  }
 
-    private void Update()
+  private void Update()
+  {
+    if (state == GameState.END)
     {
+      gameOverUI.SetActive(true);
     }
-
+  }
 }
