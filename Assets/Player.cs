0ed2b4defa4a821a3cad54d92b38eebeed5c9752
Grace <fort745@gmail.com>
2023-01-30 17:49:41 -0500

    Partially working aiming?

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,6 +8,11 @@ public class Player : MonoBehaviour
   public int infectedState;
   public Game game;
   public float maxSpeed;
+  public Vector2 startPos, endPos, direction;
+  public bool canMove;
+  private Vector3 mousePos;
+  private Camera mainCam;
+  public GameObject aimer;
 
   private void Awake()
   {
@@ -17,28 +22,63 @@ public class Player : MonoBehaviour
     game = FindObjectOfType<Game>();
   }
 
-  public void Move()
-  {
-    //sets movement
-    // get mouse position
-    Vector3 mousePos = Input.mousePosition;
-    // convert mouse position to world position
-    Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-    // get direction from player to mouse position
-    Vector3 direction = (worldPos - transform.position);
-    Vector2 direction2D = new Vector2(direction.x, direction.y).normalized;
-    // apply force in direction of mouse
-    rb.AddForce(direction2D * maxSpeed);
-  }
+    private void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        aimer.SetActive(true);
+    }
+
+    public void Move()
+    {
+        ////sets movement
+        //// get mouse position
+        //Vector3 mousePos = Input.mousePosition;
+        //// convert mouse position to world position
+        //Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        //// get direction from player to mouse position
+        //Vector3 direction = (worldPos - transform.position);
+        //Vector2 direction2D = new Vector2(direction.x, direction.y).normalized;
+        //// apply force in direction of mouse
+        //rb.AddForce(direction2D * maxSpeed);
+        canMove = true;
+
+    }
+        
+
+    private void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rotation = mousePos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        aimer.transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        if (Input.GetMouseButtonDown(0) && canMove == true)
+        {
+            aimer.SetActive(true);
+        }
+
+        if (Input.GetMouseButtonUp(0) && canMove == true)
+        {
+            print("mouse released");
+            Vector3 mousePos = Input.mousePosition;
+            // convert mouse position to world position
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            // get direction from player to mouse position
+            Vector3 direction = (worldPos - transform.position);
+            Vector2 direction2D = new Vector2(direction.x, direction.y).normalized;
+            rb.AddForce(direction * maxSpeed);
+            canMove = false;
+            aimer.SetActive(false);
+        }
+
+        //if (canMove == true)
+        //{
+        //    //show aiming line to mouse
+
+        //    aimer.SetActive(true);
+        //}
 
 
-  private void Update()
-  {
-  }
-
-  private void OnEnable()
-  {
-  }
+    }
 
 
   private void OnCollisionEnter2D(Collision2D collision)
